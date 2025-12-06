9f4eb712637d543fc969f986dd3f298a9cb3e2d2
Hakim-pixel <rahmadciamis@gmail.com>
2025-12-06 08:44:57 +0700

    update 1.5,2

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FS8kV+dI0tg7FtkEQ35+alG1zlDZDSEbqu3cai34Z+w=\"\n}"
--- .next/static/chunks/app/layout-db900fd69b1ee25d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5137:function(n,e,u){Promise.resolve().then(u.t.bind(u,876,23))},876:function(){}},function(n){n.O(0,[29,971,117,744],function(){return n(n.s=5137)}),_N_E=n.O()}]);
